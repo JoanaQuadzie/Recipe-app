8714c01335dc7e7cfaa7131adef39bc2a9f485f5
JoanaQuadzie <joanaq03@gmail.com>
2024-02-23 16:14:52 +0000

    mui techniques

---
+++ src/pages/recipes/index.jsx
@@ -0,0 +1,30 @@
+import { Card, CardContent, CardMedia, Container, Grid, TextField, Typography} from "@mui/material";
+
+export default function Recipes() {
+    return (
+        <Container sx={{ my: '2rem'}} maxWidth="sm">
+            <TextField 
+            fullWidth 
+            id="outlined-basic" 
+            label="Enter a keyword to search recipes and hit Enter" 
+            variant="outlined" />
+
+            
+            <Grid sx={{ mt: '1rem'}} container spacing={3}>
+                <Grid item xs={4}>
+                    <Card>
+                        <CardMedia 
+                        sx={{height: 140}}
+                        image="https://images.unsplash.com/photo-1526318896980-cf78c088247c?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    </Card>
+                    <CardContent>
+                        <Typography variant="h5">Ramen dish</Typography>
+                    </CardContent>    
+                </Grid>
+            </Grid>
+        </Container>
+    )
+
+}
+
+
